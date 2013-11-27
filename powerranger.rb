12712ef10250e5792d6b78549e83395e71d84ce2
LesleyMdCampo <lesliz32@gmail.com>
2013-11-27 09:53:09 -0600

    Added methods to person class for initialize, run, scream and drink_coffee.

--- powerranger.rb
+++ powerranger.rb
@@ -1,4 +1,22 @@
 class Person
+	def initialize(name, caffeine_level=200)
+		@name = name
+		@caffeine_level = caffeine_level
+	end
+
+	def run(distance)
+		@caffeine_level -= distance/20
+		puts "I am headed your way now. Good thing I had my coffee today."
+	end
+
+	def scream(exclamation)
+		puts exclamation
+	end
+
+	def drink_coffee(caffeine)
+		@caffeine_level += caffeine
+		puts "This coffee is damn good. I'm sure to be saving people soon!"
+	end
 end
 
 class PowerRanger
